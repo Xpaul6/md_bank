20520077533bb5fd376499e57b2d3e74a77c2f08
Pavel Kholkin <xpaul6@gmail.com>
2026-02-05 15:30:01 +0500

    b:feat: swagger docs autogen

--- backend/index.js
+++ backend/index.js
@@ -1,54 +1,30 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerAutogen from 'swagger-autogen';
 import fs from 'fs';
 import path from 'path';
 
-
 const app = express();
-const PORT = "8080";
+const PORT = 8080;
 
 // Swagger
 const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'MD Bank API',
-      version: '1.0.0',
-      description: 'API documentation',
-    },
-    servers: [
-      {
-        url: `http://localhost:${PORT}`,
-      },
-    ],
+  info: {
+    title: 'Dashboard API',
+    description: 'Homelab dashboard API',
   },
-  apis: ['./index.js'],
+  host: `localhost:${PORT}`
 };
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+const outputFile = './swagger-output.json';
+const routes = ['./index.js'];
+
+swaggerAutogen()(outputFile, routes, swaggerOptions);
+const swaggerFile = JSON.parse(fs.readFileSync('./swagger-output.json'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // Routes
-/**
- * @swagger
- * /server-status:
- *   get:
- *     description: Returns server status message string
- *     responses:
- *       200:
- *         description: A successful response.
- */
 app.get('/server-status', (_, res) => { return res.send('Server is up!') });
 
-/**
- * @swagger
- * /files:
- *   get:
- *     description: Returns list of avaliable files
- *     responses:
- *       200:
- *         description: A successful response.
- */
 app.get('/files', (_, res) => {
   try {
     let files = fs.readdirSync('./content/');
@@ -58,32 +34,6 @@ app.get('/files', (_, res) => {
   }
 });
 
-/**
- * @swagger
- * /read-file:
- *   get:
- *     description: Returns content of specific file
- *     parameters:
- *       - in: query
- *         name: fileName
- *         schema:
- *           type: string
- *         required: true
- *         description: The name of the file to read.
- *     responses:
- *       200:
- *         description: The file content.
- *         content:
- *           text/plain:
- *             schema:
- *               type: string
- *       400:
- *         description: Bad Request. Missing fileName query parameter.
- *       404:
- *         description: File not found.
- *       500:
- *         description: Internal Server Error.
- */
 app.get('/read-file', (req, res) => {
   const { fileName } = req.query;
 
